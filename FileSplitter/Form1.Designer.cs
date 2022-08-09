8ec3eb2e6cb9a37bf5cd714a9d33fe0c290f8c76
krayknot <krayknot@gmail.com>
2022-08-10 03:08:13 +0530

    Add files via upload

---
+++ FileSplitter/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FileSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtChunkSize = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtChunkSize);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnBrowseOutput);
+            this.groupBox1.Controls.Add(this.txtOutputDirectory);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.txtInputFile);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 177);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Provide the file details below:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File:";
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Location = new System.Drawing.Point(11, 44);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(293, 21);
+            this.txtInputFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(310, 42);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(35, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Directory:";
+            // 
+            // txtOutputDirectory
+            // 
+            this.txtOutputDirectory.Enabled = false;
+            this.txtOutputDirectory.Location = new System.Drawing.Point(11, 91);
+            this.txtOutputDirectory.Name = "txtOutputDirectory";
+            this.txtOutputDirectory.Size = new System.Drawing.Size(293, 21);
+            this.txtOutputDirectory.TabIndex = 4;
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Enabled = false;
+            this.btnBrowseOutput.Location = new System.Drawing.Point(309, 91);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(35, 23);
+            this.btnBrowseOutput.TabIndex = 5;
+            this.btnBrowseOutput.Text = "...";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Enabled = false;
+            this.btnSplit.Location = new System.Drawing.Point(199, 187);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(75, 23);
+            this.btnSplit.TabIndex = 1;
+            this.btnSplit.Text = "&Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(280, 187);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Chunk size in bytes [Integer] :";
+            // 
+            // txtChunkSize
+            // 
+            this.txtChunkSize.Enabled = false;
+            this.txtChunkSize.Location = new System.Drawing.Point(11, 140);
+            this.txtChunkSize.Name = "txtChunkSize";
+            this.txtChunkSize.Size = new System.Drawing.Size(59, 21);
+            this.txtChunkSize.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 222);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Splitter  -  Kshitij";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.TextBox txtOutputDirectory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtChunkSize;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FileSplitter/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FileSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.ShowDialog();
+            txtInputFile.Text = openFileDialog1.FileName;
+
+            txtOutputDirectory.Enabled = true;
+            btnBrowseOutput.Enabled = true;
+        }
+
+        private void btnBrowseOutput_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtOutputDirectory.Text = folderBrowserDialog1.SelectedPath;
+
+            txtChunkSize.Enabled = true;
+            btnSplit.Enabled = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(-1);
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            if (txtInputFile.Text.Length > 0 && txtOutputDirectory.Text.Length > 0 && txtChunkSize.Text.Length > 0)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                SplitFile(txtInputFile.Text.Trim(), Convert.ToInt32(txtChunkSize.Text.Trim()), txtOutputDirectory.Text.Trim());
+                Cursor.Current = Cursors.Arrow;
+
+                string message = "Split operation has completed. Open Destination folder?";
+                if (MessageBox.Show(message, "Information", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    Process.Start(txtOutputDirectory.Text.Trim());
+                }
+            }
+            else
+            {
+                MessageBox.Show("One of the Input/Output path or chunk size text box is empty.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void SplitFile1(string inputFile, int chunkSize, string path)
+        {
+            const int BUFFER_SIZE = 20 * 1024;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            using (Stream input = File.OpenRead(inputFile))
+            {
+                int index = 0;
+                while (input.Position < input.Length)
+                {
+                    using (Stream output = File.Create(path + "\\" + index + Path.GetExtension(inputFile)))
+                    {
+                        int remaining = chunkSize, bytesRead;
+                        while (remaining > 0 && (bytesRead = input.Read(buffer, 0,
+                                Math.Min(remaining, BUFFER_SIZE))) > 0)
+                        {
+                            output.Write(buffer, 0, bytesRead);
+                            remaining -= bytesRead;
+                        }
+                    }
+                    index++;
+                    Thread.Sleep(500); // experimental; perhaps try it
+                }
+            }
+        }
+
+        public bool SplitFile(string SourceFile, int nNoofFiles, string OutputPath)
+        {
+            bool Split = false;
+            try
+            {
+                FileStream fs = new FileStream(SourceFile, FileMode.Open, FileAccess.Read);
+                int SizeofEachFile = (int)Math.Ceiling((double)fs.Length / nNoofFiles);
+                for (int i = 0; i < nNoofFiles; i++)
+                {
+                    string baseFileName = Path.GetFileNameWithoutExtension(SourceFile);
+                    string Extension = Path.GetExtension(SourceFile);
+                    FileStream outputFile = new FileStream(OutputPath + "\\" + baseFileName + "." +
+                        Extension, FileMode.Create, FileAccess.Write);
+                    //mergeFolder = OutputPath;// Path.GetDirectoryName(SourceFile);
+                    int bytesRead = 0;
+                    byte[] buffer = new byte[SizeofEachFile];
+                    if ((bytesRead = fs.Read(buffer, 0, SizeofEachFile)) > 0)
+                    {
+                        outputFile.Write(buffer, 0, bytesRead);
+                        //outp.Write(buffer, 0, BytesRead);
+                        //string packet = baseFileName + "." + i.ToString().PadLeft(3, Convert.ToChar("0")) + Extension.ToString();
+                        //Packets.Add(packet);
+                    }
+                    outputFile.Close();
+                }
+                fs.Close();
+            }
+            catch (Exception Ex)
+            {
+                throw new ArgumentException(Ex.Message);
+            }
+
+            return Split;
+        }
+    }
+}
